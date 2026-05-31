c2fa1a714ffa60871f8757ed4cded32e140dad86
michalviitek <michalviitek@gmail.com>
2026-05-31 15:57:38 +0200

    Add files via upload

---
+++ texty/poezie.js
@@ -0,0 +1,36 @@
+window.poezieData = [
+  {
+      nazev: "tkanička.",
+      podnadpis: "Báseň",
+      text: `vzpomněl jsem si
+když už jsem tu skoro nebyl
+a lapal přitom po dechu
+jak jsi mi před týdnem usnula na rameni
+a já ti ve tři ráno v četl
+nemohla jsi rozumět už ani verši
+ale četl jsem dál
+a doufal
+že zůstal puštěný plynový vařič
+který jsem v bytě nikdy neměl
+nebo že začne atomová válka
+nebo že to celý tady
+nějakým
+obzvlášť potupným
+nebo obzvlášť rychlým
+způsobem
+říká se totiž
+že skončit se má v tom nejlepším
+pak ale tkanička
+sklouzla z kliky
+and já se o něco později probudil
+ve zkroucené poloze
+pod dveřmi
+zvracel jsem
+jenom ty vzpomínky
+se mi ze sebe nějak dostat nepodařilo
+takže teď nevím
+jestli už mám to nejlepší za sebou
+a pokud ne
+jestli se vyplatí ještě čekat.`
+  }
+];
