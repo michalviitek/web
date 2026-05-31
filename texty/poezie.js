c0e91e089fe64285f5e2b9af1de9e84846594230
michalviitek <michalviitek@gmail.com>
2026-05-31 18:09:08 +0200

    Update poezie.js

--- texty/poezie.js
+++ texty/poezie.js
@@ -1,36 +1,179 @@
 window.poezieData = [
   {
-      nazev: "tkanička.",
-      podnadpis: "Báseň",
-      text: `vzpomněl jsem si
+      nazev: "nikdy jsem nebyl.",
+      text: `nikdy jsem nebyl
+a zdravotní sestře jsem tím možná prodloužil 
+intimní chvilku s cigaretou
+kterou trávila 
+u zadního vchodu porodnice
+důstojně zabalena v péřové bundě
+a možná taky ne
+
+taky moje máma
+co vzhledem k okolnostem
+není moje máma
+nevidí na břiše jednu další jizvu 
+a nemá tolik vrásek na čele
+a má o pár let navíc k dobru
+
+nebyla to moje hlava
+která se neustále rozbíjela
+o asfalt
+při letu přes řídítka
+o beton
+pod skákacím hradem
+jindy za pomoci pěsti spolužáka
+o kovová dvířka šatní skříně 
+
+nebyl jsem tím důvodem
+pro který jeden kníratý šedesátník
+odcházel dřív z pracovní porady 
+a nebyl jsem svědkem hádky
+o existenci jistých vlasů
+na jeho kabátě
+
+nikdy jsem nezažil ranní zásah policie
+v rodinném domě
+co v ten čas
+už dávno nebyl obýván rodinou
+v tradičním slova smyslu
+ani v jakémkoliv jiném slova smyslu
+
+neviděl jsem jak vypadá
+vynášení rozsudku
+nad zcela jistě nevinným člověkem
+kterého znáte
+a víte
+že on by přece nikdy nic takového neudělal
+
+nikdy jsem nebyl
+což zamrzí
+mohl jsem o tom alespoň
+jednou napsat sbírku.`
+  },
+  {
+    nazev: "tkanička.",
+    text: `vzpomněl jsem si
 když už jsem tu skoro nebyl
 a lapal přitom po dechu
 jak jsi mi před týdnem usnula na rameni
-a já ti ve tři ráno v četl
-nemohla jsi rozumět už ani verši
+a já ti ve tři ráno v četl 
+
+spala jsi
 ale četl jsem dál
-a doufal
-že zůstal puštěný plynový vařič
-který jsem v bytě nikdy neměl
-nebo že začne atomová válka
+a doufal 
+že zůstal puštěný plynový vařič 
+který jsem v bytě nikdy neměl 
+nebo že začne atomová válka 
 nebo že to celý tady
 nějakým
 obzvlášť potupným
 nebo obzvlášť rychlým
-způsobem
-říká se totiž
-že skončit se má v tom nejlepším
-pak ale tkanička
+nebo obzvlášť jakýmkoliv způsobem
+skončí
+
+pak ale tkanička 
 sklouzla z kliky
-and já se o něco později probudil
-ve zkroucené poloze
-pod dveřmi
-zvracel jsem
-jenom ty vzpomínky
+a já se o něco později probudil 
+ve zkroucené poloze 
+pod dveřmi 
+
+vyzvracel jsem díru do záchodu
+jenom ty vzpomínky 
 se mi ze sebe nějak dostat nepodařilo
 takže teď nevím
 jestli už mám to nejlepší za sebou
-a pokud ne
+a pokud ne 
 jestli se vyplatí ještě čekat.`
-  }
+},
+{
+  nazev: "když přijdu domů z práce.",
+  text: `když přijdu domů z práce
+po hodinách ztracených ve veřejné dopravě
+které si nemůžu odečíst od základu daně
+a které stejně každodenně podstoupím
+lehnu na postel 
+a potím se jako prase 
+
+už se začalo topit
+a já nechal přes den
+zavřená okna
+
+popadnu knížku 
+co se válí hřbetem dolů
+na nočním stolku
+píšou v ní o sebevrazích
+ve středověku
+v evropě
+a píšou v ní taky o tom
+jak těm mrtvým chudákům
+ještě po tom 
+co už byly studený
+uřezávali hlavy
+a nebo jak je nabodávali na kůly
+a zakopávali je na křižovatkách cest
+nebo někde hodně daleko od civilizace
+a nebo je pouštěli v sudech po proudu
+jako dopisy v láhvi
+a nebo je prostě spálili 
+a pohnojili s nima pole 
+
+odložím tu knihu
+pak si to udělám
+a myslím na tebe.`
+},
+{
+  nazev: "jedno psí období.",
+  text: `bydlel jsem nějakou dobu 
+v malým rohovým pokoji
+venkovskýho domu
+s obrovskou zahradou 
+u jednoho z několika přítelů
+se kterýma máma byla
+po rozvodu s tátou
+
+tenhle text ale nemá být o tom 
+proč jsem tak nekompromisní  
+s morální stránkou osobnosti rodičů
+prostě
+pointou má být
+že pod oknem toho mýho dočasnýho rohovýho bejváku 
+na tý obrovský zahradě jednoho z máminejch přítelů 
+stál psí kotec
+
+byla to malá oplocená kůlnička 
+dva na dva metry
+s betonovou podlahou
+věčně pokrytou hovnama 
+jednou miskou na žrádlo 
+a druhou na vodu 
+ 
+toho psa nepouštěli ven
+a koukali se místo toho celej den 
+na americký seriály dabovaný jedním člověkem 
+a jenom jednou za týden shrábli ty hovna 
+a v létě 
+obden osprchovali zahradní hadicí
+podlahu 
+i toho psa 
+
+a i když pořád hrozně smrděl 
+byl to nádhernej 
+černej 
+německej 
+ovčák 
+
+trvalo to asi dva roky 
+a pak umíral 
+dlouhý týdny 
+skučel a úpěl 
+než zavolali veterináře 
+aby je i jeho zbavil trápení 
+ještě jednou ho osprchovali 
+a doplnili misku s vodou a žrádlem 
+jako by to mělo něco změnit na výsledku 
+
+tehdy jsem si připadal jako on
+a někdy si tak pořád připadám.`
+}
 ];
