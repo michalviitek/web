baec07f2ce76dd3eba2c4bd8770426e6cf2662b7
michalviitek <michalviitek@gmail.com>
2026-06-01 21:12:18 +0200

    Update poezie.js

--- texty/poezie.js
+++ texty/poezie.js
@@ -1,5 +1,323 @@
 window.poezieData = [
   {
+    nazev: "tramvajová elegie.",
+    text: `půlnoc za dveřma
+nějakým nedopatřením 
+jsem se objevil na sedačce
+v posledním nočním spoji
+meziměstský tramvaje
+
+společnost mi dělají dvě
+anonymní siluety
+na protějším konci vozu
+a jeden chlápek
+kterej do atmosféry uvolňuje
+nakyslou opilost
+a při tom škrábě 
+kapesním nožíkem 
+do sedačky před sebou 
+odkazy pro příští generace
+
+v druhý ruce drží flašku plzeňskýho
+a když ji upustí
+odkutálí se 
+mimo naše zorný pole 
+
+čeká mě
+ještě dlouhá cesta od problémů
+a na konci 
+smyčka obratiště.`
+},
+  {
+    nazev: "fastfood.",
+    text: `práce ve fastfoodu
+mi vydělávala na studium 
+a když mi náhodou zbyly nějaký peníze 
+úspěšně jsem se jich rychle zbavil
+
+nemůžu ale říct 
+že ta práce byla otročinou na mladistvejch
+né tak
+jak se o tom mezi lidma často mluví
+
+paradoxně
+nejhorší na tom všem byli zákazníci 
+byli horší než hranolky před namočením do fritézy 
+byli horší než vrstva olejový hmoty na podlaze kuchyně 
+byli horší než vzdechy studentky
+kterou při jedný směně
+defloroval ve skladě 
+jeden z manažerů
+
+málokdo v tý práci vydržel dlouho   
+ale já byl vyvolenej 
+a brzo jsem místo na kase 
+vyměnil za práci s kávovarem 
+a hleděl si svýho 
+
+sem tam se mi zalíbila nějaká pěkná holka
+co si přišla přes léto vydělat na novej telefon
+pracovní uniformu tvořily velmi uplý džíny 
+a to se mi na tý práci 
+taky moc líbilo
+
+sandra byla stejně stará jako já 
+ale působila takovým tím dospělým dojmem 
+takže jenom její přítomnost
+sváděla všechny v místnosti
+a v práci asi nebylo chlapa
+kterej by si na ní nemyslel
+kromě mě 
+navíc
+já byl tehdy takovej klučík
+
+občas se ulila od práce 
+aby se jenom opřela o skříň za barem 
+mimo dosah videokamer 
+a zatímco na mě koukala 
+vedla nenucený řeči
+který lidi vedou 
+když se chtějí s někým seznámit 
+i kdyby jenom ze zvědavosti
+
+jednou 
+než odešla domů
+nadhodila
+že někdy musíme někam zajít 
+a obejmula mě
+přitiskla na mě celou svojí krásu 
+a já všechno cítil
+a ona taky musela všechno cítit 
+vždycky jsem měl za to
+že „někdy“ a „někam“
+znamená ve skutečnosti „nikdy“ a „nikam“
+
+po roce jsme spolu vedli další rozhovor
+už byla zase zadaná
+ale prozradila mi
+že jsem se jí tehdy líbil 
+
+já 
+
+z celý posraný restaurace 
+nevěděl jsem co na to říct 
+
+pak mi ještě prozradila 
+že nechápe
+jak jsem si nemohl nevšimnout těch náznaků
+a já to dneska taky nechápal
+rozhovor skončil a ona šla za svojí prací
+už to tehdy byla manažerka 
+a já sledoval její neuvěřitelně hustý a rudě hnědý vlasy 
+jak se jí pohupujou nad zadkem
+a obrys jejího těla se nadobro vzdaloval 
+v kuchyňským oparu.`
+},
+  {
+    nazev: "mezi druhou a třetí ráno.",
+    text: `mezi druhou a třetí ráno 
+když konečně všichni usnou 
+ti šťastní sami 
+ti šťastnější
+s kočičí řiťkou přilepenou u těla
+a přestanou očekávat 
+přestanou chtít 
+přestanou vnímat
+sednu si a píšu
+beze strachu
+že by mě vyrušili 
+
+vyhlížím z okna
+a konečně 
+nic nevidím 
+potichu
+zato s otevřenou pusou 
+zívám
+a z nudy si rozškrábu strup na nártu
+
+jsem sám 
+a mezi druhou a třetí ráno
+si na chvilku připadám jako živá bytost. `
+},
+  {
+    nazev: "v pokoji sedí starý mrtvý muž.",
+    text: `v pokoji sedí starý mrtvý muž
+a nenachází slova
+pro poruchu mozku
+
+slyší 
+ale nerozumí proč 
+když jeho žena 
+nadává u večerních zpráv 
+a už jí nikdy neprozradí 
+kolikrát 
+a s kým
+ji před lety podvedl 
+
+umírá 
+říkají 
+jenže on si moc dobře uvědomuje
+že umřel už před časem
+kdy mu došla slova 
+když přestal být součástí diskuzí 
+při rodinných obědech 
+když nedokázal prohodit pár slov
+s jeho spolužákem z malotřídky
+kterého náhodou potkal cestou na poštu
+kam ho jeho žena doprovázela
+
+umírá
+říkají
+umírá na stáří 
+na milosrdnost 
+kterou mu život nadělí před osmdesátkou
+po tom co ho v sedmdesáti zbavil života 
+
+než mu bylo dáno 
+uměl dvě poslední slova
+která opakoval ještě z nemocniční postele
+poslední den 
+kdy se mu ulevilo 
+a napil se naposledy lahvovýho 
+
+ahoj
+kurva 
+
+to první slovo
+asi mělo co dočinění s tím
+jak často jsem ho navštěvoval 
+to druhé slovo
+asi mělo co dočinění i s tím 
+kolik času trávil s tou ženou
+která nadávala 
+u večerních zpráv.`
+},
+  {
+    nazev: "kličkování v první letošní bouřce.",
+    text: `poezie je mrtvá
+bůh je mrtvej 
+já jsem pro spousta lidí taky mrtvej
+pro sebe občas taky 
+nebo bych alespoň nejradši byl 
+
+vidím to tak
+když se dívám do slunce
+do dne
+odcházejícího za horizont
+
+napadá to na mě
+jako kapky první letošní bouřky
+když se mezi nima naivně snažím kličkovat
+cestou z jednoho místa přečkávání 
+ke druhýmu
+a nepřečkávám zdaleka 
+jenom ten déšť
+
+měl bych si koupit deštník
+a k tomu nějakou kurvu 
+a nový kalhoty 
+bez díry na zadní kapse.`
+},
+{
+  nazev: "abychom si rozuměli.",
+  text: `zjišťuju
+že jsem na ten zmačkanej papír
+napsal úplně jiný věci 
+než jaký teď čteš
+
+nemysli si 
+to že dýcháme stejnej vzduch 
+že polykáme stejnej déšť
+že na nás svítí shůry 
+to samý světlo
+neznamená
+že píšu o tvým životě
+
+nejsou to tvoje problémy 
+ani jsem nespal s tvojí holkou
+není to tvoje rodina 
+tvoje práce
+ani tvoje modřina na krku
+po oprátce
+
+až se na tebe usměje štěstí
+nebo krásná zrzka v autobuse
+a když se ti z toho podlomí kolena
+nehledej spasení
+v textu od neznámýho básníka
+
+utíkáš mezi řádky
+ale nevyčetl jsi z nich
+že nejdřív musíš žít
+aby to všechno 
+začalo dávat smysl.`
+},
+{
+  nazev: "umění sázet.",
+  text: `občas sázím
+hlavně když se koná nějakej větší turnaj
+smíšených bojových umění 
+a řeknu vám 
+umění
+je na to hlavně sázet
+i když myslím že se celkem vyznám
+není to matematika 
+mezi provazy 
+nebo v kleci 
+prostě jedna plus jedna 
+nedává dohromady 
+dva
+veterán s bilancí 39-7
+může prohrát
+klidně už v prvním kole
+proti mladý krvi s 8-2
+někdy je smutný
+dívat se na to 
+jak k zemi padají desítky let zkušeností
+pod jedním dobře trefeným úderem 
+
+sem tam
+dám tipy k sázení i tátovi 
+vedle počasí
+přikrášlených vzpomínek
+a politiky
+je sázení naše jediný společný téma k hovoru 
+když se jednou za čas
+náhodou potkáme
+
+je to průměrně placený důchodce
+co si jezdí po světě
+a vždycky mi přiveze magnetku na lednici
+nebo propisku 
+naposled na sobě měla logo
+jednoho známého fotbalového klubu
+takže i když si nestěžuju na inflaci 
+a on ano
+má se oproti mě pořád dobře
+já mám před sebou snad jenom o něco víc času
+což teda taky není žádný terno
+
+za tu dobu
+co mu radím se sázkama
+vyhrál dost peněz
+a já zklamávám
+tak jednou
+do dvaceti případů
+
+jemu ale dávám jistoty
+přičemž sám vsadím každou blbost
+
+až jednou začnu sázet
+na ty dvě tři tutovky 
+mezi kurzem 1,5 a 3,2
+třeba se do tý barcelony 
+taky jednou podívám 
+i když bych radši na sever
+táta radši na jih
+a to je ten rozdíl 
+mezi náma.`
+},
+  {
       nazev: "nikdy jsem nebyl.",
       text: `nikdy jsem nebyl
 a zdravotní sestře jsem tím možná prodloužil 
